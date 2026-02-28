f493fdf0054db129a2e101a0598fd601c075383a
Gaurav Dogra <gauravdogra2005june@gmail.com>
2026-02-28 12:46:30 +0530

    Error in schema

--- backend/src/prisma/client.ts
+++ backend/src/prisma/client.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL!;
+
+const pool = new Pool({
+  connectionString,
+});
+
+const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === "development"
-    ? ["query", "info", "warn", "error"]
-    : ["error"]
+  adapter,
 });
