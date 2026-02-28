5624f25f2a6aa5eee48758be668994f02caed442
Gaurav Dogra <gauravdogra2005june@gmail.com>
2026-02-28 13:44:06 +0530

    error fix

--- frontend/src/hooks/useInventory.ts
+++ frontend/src/hooks/useInventory.ts
@@ -1,13 +1,13 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import { getInventory } from "@/services/inventory.service";
+import { getInsightsOverview } from "@/services/inventory.service";
 import { InventoryItem } from "@/types/inventory.types";
 
 export const useInventory = () => {
   return useQuery<InventoryItem[]>({
     queryKey: ["inventory"],
-    queryFn: getInventory,
+    queryFn: getInsightsOverview,
     staleTime: 1000 * 60 * 5, // 5 minutes cache
   });
 };
