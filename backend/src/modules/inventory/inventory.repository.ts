cb24fd49b39721ede812c2c0a053d70bae67d695
Gaurav Dogra <gauravdogra2005june@gmail.com>
2026-03-02 15:21:17 +0530

    Fixed inventory repo

--- backend/src/modules/inventory/inventory.repository.ts
+++ backend/src/modules/inventory/inventory.repository.ts
@@ -2,6 +2,10 @@ import { prisma } from "../../prisma/client";
 
 export const findInventoryByOrganization = (organizationId: string) => {
   return prisma.inventory.findMany({
-    where: { organizationId }
+    where: { organizationId },
+    include: {
+      sku: true,
+      store: true, // optional but good to include
+    },
   });
 };
